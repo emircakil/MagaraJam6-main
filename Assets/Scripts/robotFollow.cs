aa7131c08b293675f20a9338ae6587f6a1ac7467
Emirhan Aydin Cakil <emir76aydin44@gmail.com>
2023-11-18 12:56:45 +0300

    Drone calisiliyor

--- Assets/Scripts/robotFollow.cs
+++ Assets/Scripts/robotFollow.cs
@@ -9,16 +9,47 @@ public class robotFollow : MonoBehaviour
     [SerializeField] NavMeshAgent robot;
     [SerializeField] Transform player;
     [SerializeField] Animator anim;
-    void Start()
+    Rigidbody rb;
+    float speed;
+    float timer = 10f;
+
+
+    void Awake()
     {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+     
         
     }
+   
 
     void Update()
     {
-        if (anim.GetBool("Open_Anim") == false)
+        speed = Vector3.Magnitude(rb.velocity);
+        robot.SetDestination(player.position);
+            
+
+        if (timer > 0)
         {
-            robot.SetDestination(player.position);
+
+            timer -= Time.deltaTime;
+
         }
+
+        if (timer <= 0)
+        {
+
+            anim.SetBool("OpenAnim", false);
+        }
+
+        if (speed > 0 && !anim.GetBool("OpenAnim"))
+        {
+            anim.SetBool("WalkAnim", true);
+        }
+        else {
+
+            anim.SetBool("WalkAnim", false);
+        }
+
     }
 }
