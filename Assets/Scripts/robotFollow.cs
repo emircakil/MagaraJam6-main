c0903018bb85a12f455538394f26fe1fdb22c9ae
Emirhan Aydin Cakil <emir76aydin44@gmail.com>
2023-11-18 15:56:59 +0300

    Robot tamam

--- Assets/Scripts/robotFollow.cs
+++ Assets/Scripts/robotFollow.cs
@@ -6,49 +6,15 @@ using UnityEngine.AI;
 public class robotFollow : MonoBehaviour
 {
 
-    [SerializeField] NavMeshAgent robot;
+  
     [SerializeField] Transform player;
-    [SerializeField] Animator anim;
-    float speed;
-    float timer = 10f;
-
-
-    void Awake()
-    {
- 
-        anim = GetComponent<Animator>();
-     
-        
-    }
-   
+  
+  
 
     void Update()
     {
-  
-        robot.SetDestination(player.position);
-            
-
-        if (timer > 0)
-        {
-
-            timer -= Time.deltaTime;
-
-        }
-
-        if (timer <= 0)
-        {
-
-            anim.SetBool("OpenAnim", false);
-        }
-
-        if (speed > 0 && !anim.GetBool("OpenAnim"))
-        {
-            anim.SetBool("WalkAnim", true);
-        }
-        else {
-
-            anim.SetBool("WalkAnim", false);
-        }
-
+      
+        
+     
     }
 }
---
+++ Assets/Third-Party/RobotSphere/Assets/Scripts/RobotFreeAnim.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+
+public class RobotFreeAnim : MonoBehaviour {
+
+	
+	Animator anim;
+	[SerializeField] Transform player;
+    [SerializeField] NavMeshAgent robot;
+
+    // Use this for initialization
+    void Awake()
+	{
+		anim = gameObject.GetComponent<Animator>();
+      //  gameObject.transform.eulerAngles = player.position;
+
+    }
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		if (!anim.GetCurrentAnimatorStateInfo(0).IsName("anim_open"))
+		{
+			CheckKey();
+			//	gameObject.transform.eulerAngles = player.position;
+
+			gameObject.transform.LookAt(player.position);
+
+			robot.SetDestination(player.position);
+		}
+    }
+
+	void CheckKey()
+	{
+		// Walk
+	
+		if (Keyboard.current.wKey.IsPressed() ||
+			Keyboard.current.aKey.IsPressed() ||
+			Keyboard.current.dKey.IsPressed() ||
+            Keyboard.current.sKey.IsPressed())
+
+
+        {
+            anim.SetBool("Walk_Anim", true);
+		}
+		else 
+		{
+			anim.SetBool("Walk_Anim", false);
+		}
+
+		
+
+		// Roll
+		if (Keyboard.current.shiftKey.IsPressed())
+		{
+			if (anim.GetBool("Roll_Anim"))
+			{
+				anim.SetBool("Roll_Anim", false);
+			}
+			else
+			{
+				anim.SetBool("Roll_Anim", true);
+			}
+		}
+
+		// Close
+			
+		if (Keyboard.current.ctrlKey.IsPressed())
+		{
+			if (!anim.GetBool("Open_Anim"))
+			{
+				anim.SetBool("Open_Anim", true);
+			}
+			else
+			{
+				anim.SetBool("Open_Anim", false);
+			}
+		}
+	}
+
+}
