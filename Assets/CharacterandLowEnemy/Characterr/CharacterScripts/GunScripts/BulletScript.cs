72550bccf77a72ed0a2bb0224960a5012f5bb391
21-82 <geyikilyas8@gmail.com>
2023-11-19 16:40:46 +0300

    seseklendi

--- Assets/CharacterandLowEnemy/Characterr/CharacterScripts/GunScripts/BulletScript.cs
+++ Assets/CharacterandLowEnemy/Characterr/CharacterScripts/GunScripts/BulletScript.cs
@@ -4,14 +4,10 @@ namespace CharacterandLowEnemy.Characterr.CharacterScripts.GunScripts
 {
     public class BulletScript : MonoBehaviour
     {
-        public static BulletScript instance;
         [SerializeField] private float speed = 10f; // Mermi hızı
         [SerializeField] private float lifeTime = 5f; // Mermi maksimum menzil
-        public float bulletDamage = 23f;// Mermi hasarı
-        private void Awake()
-        {
-            instance = this;
-        }
+        [SerializeField] private float bulletDamage = 2f;// Mermi hasarı
+
         void Update()
         {
             // Mermiyi ileri doğru hareket ettir
